3b64a88cfd245f22db19703c857f54e065d5cc8b
Ian Lyttle <ian.lyttle@schneider-electric.com>
2017-01-07 16:45:22 -0600

    get coverage to 100?!?

--- tests/testthat/test-locale.R
+++ tests/testthat/test-locale.R
@@ -1,7 +1,12 @@
 context("locale")
 
-logical_format_test <- list(true = "foo", false = "bar")
+logical_format_test <- list(true = "true", false = "false")
 duration_unit_test <- "ms"
+
+default_locale_test <- list(
+  logical_format = logical_format_test,
+  duration_unit = duration_unit_test
+)
 
 test_that("logical_format works", {
   expect_error(locale(logical_format = "foo"), "list")
@@ -20,4 +25,8 @@ test_that("duration_unit works", {
     locale(duration_unit = duration_unit_test)[["duration_unit"]],
     duration_unit_test
   )
+})
+
+test_that("default_locale works", {
+  expect_identical(default_locale(), default_locale_test)
 })
